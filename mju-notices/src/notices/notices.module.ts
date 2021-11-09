1a0cfb3ad8145ad8068ca7f47401ae09a88af4c9
hou27 <ataj125@gmail.com>
2021-11-09 11:03:19 +0000

    service

--- mju-notices/src/notices/notices.module.ts
+++ mju-notices/src/notices/notices.module.ts
@@ -5,7 +5,7 @@ import { NoticeService } from './notices.service';
 import { Notice, NoticeSchema } from './schemas/notice.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: Notice.name, schema: NoticeSchema }])],
+    imports: [MongooseModule.forFeature([{ name: 'Notice', schema: NoticeSchema }])],
     providers: [NoticeResolver, NoticeService],
 	exports: [],
 })
--- mju-notices/src/notices/notices.service.ts
+++ mju-notices/src/notices/notices.service.ts
@@ -1,5 +1,9 @@
+import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Notice, NoticeDocument } from './schemas/notice.schema';
 
 @Injectable()
 export class NoticeService {
+    constructor(@InjectModel('Notice') private readonly NoticeModel: Model<NoticeDocument>) {}
 }
