29537befac3d0a7f6fb79f75568038477cc698d2
hou27 <ataj125@gmail.com>
2021-11-10 11:17:25 +0000

    connect to db

--- mju-notices/src/app.module.ts
+++ mju-notices/src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { NoticesModule } from './notices/notices.module';
 
 @Module({
-  imports: [MongooseModule.forRoot(`mongodb+srv://jalapeno: ${process.env.DB_PW}@cluster211105.liaeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`), NoticesModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(
+      `mongodb+srv://jalapeno:${encodeURIComponent(
+        process.env.DB_PW,
+      )}@cluster211105.liaeo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`,
+    ),
+    NoticesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
